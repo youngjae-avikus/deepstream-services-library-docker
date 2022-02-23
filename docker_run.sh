32f1b5761f9d466b5313545c956fb0ae366d6107
rjhowell44 <rjhowell44@gmail.com>
2022-02-23 13:36:17 -0800

    Update docker_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -13,6 +13,6 @@ sudo docker run \
     -v /tmp/argus_socket:/tmp/argus_socket \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -v ${HOME}/Downloads:/output \
-    -w /opt \
+    -w /opt/prominenceai/deepstream-services-library \
     dsl.alpha:latest
 
