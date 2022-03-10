e7dad777197051fb18f99f637e27feeed7f9cabe
rjhowell44 <rjhowell44@gmail.com>
2022-03-09 19:48:39 -0800

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # First update os
 # Setup the distribution to pick a specific version of Docker
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-   && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - 
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
 
 sudo apt-get update
-sudo apt-get install nvidia-docker2=2.8.0-1rm -rf  docker.io_19.03.6-0ubuntu1~18.04.3_arm64.deb
+sudo apt-get install nvidia-docker2=2.8.0-1
 
-# Install pip(s) and setuptoos module
+# Ensure all dependencies have been installed
 sudo apt-get install -y python3-pip
 sudo apt-get install -y python-pip
 sudo apt-get install -y python3-setuptools
@@ -16,4 +16,3 @@ sudo apt-get install -y python3-setuptools
 # Install Docker compose
 sudo apt-get install curl libffi-dev python-openssl libssl-dev zlib1g-dev gcc g++ make -y
 sudo pip3 install docker-compose
-sudo docker-compose --version
