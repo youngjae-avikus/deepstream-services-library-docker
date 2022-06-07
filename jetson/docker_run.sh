84721ec63abfc76db7ff6ba36ff9ab5f2bf21bca
youngjae-avikus <youngjae.you@avikus.ai>
2022-06-07 19:12:33 +0900

    add alpha version Dockerfile for dgpu

---
+++ jetson/docker_run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -eu
+
+xhost +
+
+docker run \
+    -it \
+    --rm \
+    --net=host \
+    --runtime nvidia \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/argus_socket:/tmp/argus_socket \
+    -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    -v /tmp/.dsl/:/tmp/.dsl \
+	dsl:dgpu
