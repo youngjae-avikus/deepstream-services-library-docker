7a6b42f2d0e045aa8df96b906ebf6ee561a0aab3
youngjae-avikus <youngjae.you@avikus.ai>
2022-06-14 14:22:22 +0900

    add ds50 branch

--- dgpu/docker_run.sh
+++ dgpu/docker_run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+ROOT_DIR=$1
+
 set -eu
 
 xhost +
@@ -13,4 +15,8 @@ docker run \
     -v /tmp/argus_socket:/tmp/argus_socket \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
     -v /tmp/.dsl/:/tmp/.dsl \
-	dsl:dgpu
+	-v ${ROOT_DIR}/deepstream-services-library/:/opt/dsl/deepstream-services-library/ \
+	-v ${ROOT_DIR}/DeepStream-Yolo:/opt/dsl/DeepStream-Yolo/ \
+	-v ${ROOT_DIR}/yolov5:/opt/dsl/yolov5 \
+	-v ${ROOT_DIR}/aiboat-nas-jetson-inference/data:/opt/dsl/data \
+	dsl:dgpu_v0.0.1
